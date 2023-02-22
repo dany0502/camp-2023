fc6f910114f355e2dd465acea79735d5488fbaf6
Harumitsu <dany.ubi@gmail.com>
2023-02-22 21:48:47 +0900

    update script

--- src/create-pr.mjs
+++ src/create-pr.mjs
@@ -1,16 +1,31 @@
 #!/usr/bin/env zx
 
+// シェルスクリプトの標準出力を無効にする
+$.verbose = false;
 const BASE_BRANCH = "main";
-const REVIEWER = "dany0502";
 const ASSIGNEE = "@me";
-let bodyMessage = "SSIA \n\n https://example.com/";
-let title = "Title";
+
 const branch = await $`git branch --show-current`;
-const tickets = branch.toString().match(/exs-[0-9]{3,4}/gi);
-if (tickets.length) {
-  title = `[${tickets[0].toUpperCase()}] ${argv.m || ""}`;
-  bodyMessage += tickets[0].toLowerCase();
+try {
+  await $`git branch -r | grep ${branch}`;
+} catch {
+  console.error("ブランチをリモートにプッシュしてください。");
+  await $`exit 1`;
 }
+
+const input = await question("PRのタイトルを入力してください。");
+
+let bodyMessage = "SSIA \n\n";
+let title = "";
+const tickets = branch.toString().match(/exs-[0-9]{3,4}/gi);
+const ticketId = tickets.length ? tickets[0] : null;
+if (ticketId !== null) {
+  title = `[${ticketId.toUpperCase()}] ${input}`;
+  bodyMessage += `https://example.com/${ticketId.toLowerCase()}`;
+} else {
+  title = `[NO-ISSUE] ${title}`;
+}
+
 const pr =
   await $`gh pr create --title ${title} --body ${bodyMessage} --assignee ${ASSIGNEE} --base ${BASE_BRANCH}`;
 await $`open ${pr}`;
