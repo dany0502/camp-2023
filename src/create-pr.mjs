a4fd7fcc5e70f6955d2fb612829e45ef3d1ab88d
Harumitsu <dany.ubi@gmail.com>
2023-02-22 22:05:33 +0900

    update script

--- src/create-pr.mjs
+++ src/create-pr.mjs
@@ -15,17 +15,18 @@ try {
 
 const input = await question("PRのタイトルを入力してください。");
 
-let bodyMessage = "SSIA \n\n";
-let title = "";
 const tickets = branch.toString().match(/exs-[0-9]{3,4}/gi);
 const ticketId = tickets.length ? tickets[0] : null;
-if (ticketId !== null) {
-  title = `[${ticketId.toUpperCase()}] ${input}`;
-  bodyMessage += `https://example.com/${ticketId.toLowerCase()}`;
-} else {
-  title = `[NO-ISSUE] ${title}`;
-}
+
+const BODY_MESSAGE =
+  ticketId !== null
+    ? `SSIA \n\nhttps://example.com/${ticketId.toLowerCase()}`
+    : "SSIA \n\n";
+const TITLE =
+  ticketId !== null
+    ? `[${ticketId.toUpperCase()}] ${input}`
+    : `[NO-ISSUE] ${title}`;
 
 const pr =
-  await $`gh pr create --title ${title} --body ${bodyMessage} --assignee ${ASSIGNEE} --base ${BASE_BRANCH}`;
+  await $`gh pr create --title ${TITLE} --body ${BODY_MESSAGE} --assignee ${ASSIGNEE} --base ${BASE_BRANCH}`;
 await $`open ${pr}`;
